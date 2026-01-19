64281cc562a79c078455958b13126cb2007d7241
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2026-01-19 23:37:27 +0800

    Level 1: Add echo functionality

--- src/main/java/Miffy.java
+++ src/main/java/Miffy.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Miffy {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Miffy\n" + "What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner myScanner = new Scanner(System.in);
+        String userInput;
+
+        System.out.println("  ____________________________________________________________");
+        System.out.println("  Hello! I'm Miffy ^_^");
+        System.out.println("  What can I do for you?");
+        System.out.println("  ____________________________________________________________");
+
+        while (true) {
+            userInput = myScanner.nextLine();
+            System.out.println("  ____________________________________________________________");
+
+            if (userInput.equals("bye")) {
+                System.out.println("  Bye. Hope to see you again soon!");
+                System.out.println("  ____________________________________________________________");
+                break;
+            }
+
+            System.out.println("  " + userInput);
+            System.out.println("  ____________________________________________________________");
+        }
+
     }
 }
