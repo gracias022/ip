27458ae5d7758569c71e5f7f5fb9b2707b2eab77
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2026-01-20 19:15:22 +0800

    Level 4: Add support for tracking Todos, Deadlines and Events

--- src/main/java/Miffy.java
+++ src/main/java/Miffy.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
 public class Miffy {
-    private static void delimiter() {
-        System.out.println("  ____________________________________________________________");
+    private TaskList taskList;
+    private Scanner scanner;
+
+    public Miffy() {
+        taskList = new TaskList();
+        scanner = new Scanner(System.in);
     }
 
     public static void main(String[] args) {
-        TaskList taskList = new TaskList();
-
-        Scanner myScanner = new Scanner(System.in);
+        Miffy miffy = new Miffy();
         String userInput;
 
         delimiter();
@@ -17,7 +19,7 @@ public class Miffy {
         delimiter();
 
         while (true) {
-            userInput = myScanner.nextLine();
+            userInput = miffy.scanner.nextLine();
             delimiter();
 
             if (userInput.equals("bye")) {
@@ -25,42 +27,88 @@ public class Miffy {
                 delimiter();
                 break;
             } else if (userInput.equals("list")) {
-                taskList.list();
+                miffy.taskList.list();
                 delimiter();
                 continue;
             } else if (userInput.startsWith("mark ")) { // include check for space after mark
                 try {
                     int index = Integer.parseInt(userInput.substring(5));
-                    Task task = taskList.markAsDone(index); // 1-based index
+                    Task task = miffy.taskList.markAsDone(index); // 1-based index
                     System.out.println("  Nice! I've marked this task as done:");
                     System.out.println("  " + task);
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println("  Oops! That task number doesn’t exist.");
                 } catch (Exception e) {
                     System.out.println("  Invalid input format: Please use 'mark <index>'");
-                } finally {
-                    delimiter();
                 }
+                delimiter();
+
             } else if (userInput.startsWith("unmark ")) {
                 try {
                     int index = Integer.parseInt(userInput.substring(7));
-                    Task task = taskList.unmark(index);
+                    Task task = miffy.taskList.unmark(index);
                     System.out.println("  OK, I've marked this task as not done yet:");
                     System.out.println("  " + task);
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println("  Oops! That task number doesn’t exist.");
                 } catch (Exception e) {
                     System.out.println("  Invalid input format: Please use 'unmark <index>'");
-                } finally {
-                    delimiter();
                 }
-            } else {
-                taskList.add(new Task(userInput));
-                System.out.println("  added: " + userInput);
                 delimiter();
+            } else if (userInput.startsWith("todo ")) {
+                Todo task = new Todo(userInput.substring(5));
+                miffy.taskList.add(task);
+                printAddConfirmation(task, miffy.taskList);
+            } else if (userInput.startsWith("deadline ")) {
+                addDeadline(userInput.substring(9), miffy.taskList);
+            } else if (userInput.startsWith("event ")) {
+                addEvent(userInput.substring(6), miffy.taskList);
             }
 
         }
 
+    }
+
+    private static void addDeadline(String input, TaskList tl) {
+        try {
+            String[] arr = input.split(" /by ");
+            if (arr.length != 2) {
+                throw new Exception();
+            }
+            Deadline task = new Deadline(arr[0], arr[1]);
+            tl.add(task);
+            printAddConfirmation(task, tl);
+        } catch (Exception e) {
+            System.out.println("  Invalid input format: Please use deadline <desc> /by <date/time>");
+        }
+    }
+
+    private static void addEvent(String input, TaskList tl) {
+        try {
+            int div1 = input.indexOf(" /from ");
+            int div2 = input.indexOf(" /to ");
+            if (div1 == -1 || div2 == -1) {
+                throw new Exception();
+            }
+            String desc = input.substring(0, div1);
+            String from = input.substring(div1 + 7, div2);
+            String to = input.substring(div2 + 5);
+            Event task = new Event(desc, from, to);
+            tl.add(task);
+            printAddConfirmation(task, tl);
+        } catch (Exception e) {
+            System.out.println("  Invalid input format: Please use event <desc> /from <start> /to <end>");
+        }
+    }
+
+    private static void printAddConfirmation(Task t, TaskList tl) {
+        System.out.println("  Got it. I've added this task:");
+        System.out.println("  " + t);
+        System.out.printf("  Now you have %d tasks in the list.\n", tl.getTaskCount());
+        delimiter();
+    }
+
+    private static void delimiter() {
+        System.out.println("  ____________________________________________________________");
     }
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -40,4 +40,8 @@ public class TaskList {
         task.unmark();
         return task;
     }
+
+    public int getTaskCount() {
+        return taskCount;
+    }
 }
