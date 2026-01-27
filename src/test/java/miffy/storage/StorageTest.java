4d7376abed04cf276cde0cb968c5d8fb1574fdc0
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2026-01-27 21:32:37 +0800

    A-JUnit: Add tests for Storage.java

---
+++ src/test/java/miffy/storage/StorageTest.java
@@ -0,0 +1,148 @@
+package miffy.storage;
+
+import miffy.task.Deadline;
+import miffy.task.Event;
+import miffy.task.Task;
+import miffy.task.Todo;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StorageTest {
+
+    @Test
+    public void parseTaskFromLine_Todo_createsTodoTask() {
+        Task t = Storage.parseTaskFromLine("T | 0 | read");
+
+        assertInstanceOf(Todo.class, t);
+        assertFalse(t.isDone());
+        assertEquals("[T][ ] read", t.toString());
+    }
+
+    @Test
+    public void parseTaskFromLine_doneTodo_createsDoneTodoTask() {
+        Task t = Storage.parseTaskFromLine("T | 1 | read");
+
+        assertInstanceOf(Todo.class, t);
+        assertTrue(t.isDone());
+        assertEquals("[T][X] read", t.toString());
+    }
+
+    @Test
+    public void parseTaskFromLine_invalidTaskType_throwsException() {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> {
+            Storage.parseTaskFromLine("X | 0 | blah");
+        });
+
+        assertEquals("Unknown task type: X", e.getMessage());
+    }
+
+    @Test
+    public void parseTaskFromLine_Deadline_createsDeadlineTask() {
+        Task t = Storage.parseTaskFromLine("D | 0 | return book | 2026-01-17T18:00");
+
+        assertInstanceOf(Deadline.class, t);
+        assertFalse(t.isDone());
+        assertEquals("[D][ ] return book (by: Jan 17 2026 6:00 PM)", t.toString());
+    }
+
+    @Test
+    public void parseTaskFromLine_doneDeadline_createsDoneDeadlineTask() {
+        Task t = Storage.parseTaskFromLine("D | 1 | return book | 2026-01-17T18:00");
+
+        assertInstanceOf(Deadline.class, t);
+        assertTrue(t.isDone());
+        assertEquals("[D][X] return book (by: Jan 17 2026 6:00 PM)", t.toString());
+    }
+
+    @Test
+    public void parseTaskFromLine_Event_createsEventTask() {
+        Task t = Storage.parseTaskFromLine("E | 0 | meeting | 2026-01-17T16:00 | 2026-01-17T18:00");
+
+        assertInstanceOf(Event.class, t);
+        assertFalse(t.isDone());
+        assertEquals("[E][ ] meeting (from: Jan 17 2026 4:00 PM to: Jan 17 2026 6:00 PM)", t.toString());
+    }
+
+    @Test
+    public void parseTaskFromLine_doneEvent_createsDoneEventTask() {
+        Task t = Storage.parseTaskFromLine("E | 1 | meeting | 2026-01-17T16:00 | 2026-01-17T18:00");
+
+        assertInstanceOf(Event.class, t);
+        assertTrue(t.isDone());
+        assertEquals("[E][X] meeting (from: Jan 17 2026 4:00 PM to: Jan 17 2026 6:00 PM)", t.toString());
+    }
+
+    @Test
+    public void parseTaskFromLine_corruptedLine_throwsException() {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> {
+            Storage.parseTaskFromLine("T | 0 sth");
+        });
+
+        assertEquals("Corrupted line: T | 0 sth", e.getMessage());
+    }
+
+    @Test
+    public void saveThenLoad_validTasks_tasksPreserved() throws Exception {
+        Path tempFile = Files.createTempFile("storage-test", ".txt");
+        Storage storage = new Storage(tempFile.toString());
+
+        ArrayList<Task> tasks = new ArrayList<>();
+        tasks.add(new Todo("read"));
+        tasks.add(new Deadline(
+                "work",
+                LocalDateTime.of(2026, 1, 17, 18, 0)));
+
+        storage.save(tasks);
+        ArrayList<Task> loaded = storage.load();
+
+        assertEquals(2, loaded.size());
+        assertEquals("[T][ ] read", loaded.get(0).toString());
+        assertEquals("[D][ ] work (by: Jan 17 2026 6:00 PM)", loaded.get(1).toString());
+
+        Files.deleteIfExists(tempFile); // clean up
+    }
+
+    @Test
+    void load_fileDoesNotExist_createsFileAndReturnsEmptyList() throws Exception {
+        Path tempFile = Files.createTempFile("storage-test", ".txt");
+        Files.delete(tempFile); // ensure file is missing
+
+        Storage storage = new Storage(tempFile.toString());
+
+        ArrayList<Task> loaded = storage.load();
+
+        assertTrue(Files.exists(tempFile));
+        assertTrue(loaded.isEmpty());
+
+        Files.deleteIfExists(tempFile); // clean up
+    }
+
+
+
+    @Test
+    public void load_fileWithCorruptedLine_lineSkipped () throws Exception {
+        Path tempFile = Files.createTempFile("storage-test", ".txt");
+
+        try (FileWriter fw = new FileWriter(tempFile.toString())) {
+            fw.write("T | 0 | read\n");
+            fw.write("bad line\n");
+        }
+
+        Storage storage = new Storage(tempFile.toString());
+
+        ArrayList<Task> loaded = storage.load();
+
+        assertEquals(1, loaded.size());
+        assertEquals("[T][ ] read", loaded.get(0).toString());
+
+        Files.deleteIfExists(tempFile); // clean up
+    }
+
+}
